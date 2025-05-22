4d6d9126a6ff9f30988900ac8674b305f9086700
Kkarthik2501 <karthiksonu25@gmail.com>
2025-05-22 12:07:21 +0530

    Implemented PrintPdf api

--- server/routes/healthRoutes.js
+++ server/routes/healthRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { healthCheck, readyCheck, serverCheck } = require("../controllers/healthCheckController");
+const { healthCheck, readyCheck, serverCheck, getPDF } = require("../controllers/healthCheckController");
 const router = express.Router();
 
 router.get("/health", healthCheck);
 router.get("/ready", readyCheck);
+
+router.post("/getPDF", getPDF);
 router.get("/", serverCheck);
+
 
 
 module.exports = router;
